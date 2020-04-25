e8e3fc62b81f1f4c087284a0bde105887aa70dfb
VBelogrudov <forledwatch@gmail.com>
2020-04-25 19:19:22 +0300

    remove missprint

--- src/main/java/crawler/Main.java
+++ src/main/java/crawler/Main.java
@@ -1,28 +1,5 @@
 package crawler;
 
-import org.jsoup.Jsoup;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URISyntaxException;
-import java.util.Arrays;
-import java.util.stream.Collectors;
-
-/**
- * В приложение необходимо встроить ролевую модель.
- * Пользователь обладает атрибутами: имя, пароль, электронная почта, список ролей. Пользователь должен иметь уникальные ключи по полям: имя пользователя и электронная почта.
- * Должен быть реализован CRUD для необходимых сущностей, а также поиск пользователя по имени и/или электронной почте.
- * <p>
- * Количество необходимых таблиц и параметров определяется разработчиком самостоятельно!
- * Ролевая модель должна встраивается в существующее решение и поэтому при реализации необходимо пользоваться следующими шаблонами проектирования:
- * • Domain Model (Подсказка: @Entity, @Table, @Id, @Column, @JoinColumn)
- * • Repository (Подсказка: JPA, @Repository, @Query)
- * • Service Layer (@Service, @Component, @Transactional)
- * <p>
- * copy: belyankin.v.s@sberbank.ru
- */
-
 /**
  * Требуется написать многопоточный HTTP-crawler.
  * Crawler должен принимать на вход URL сайта и глубину сканирования.
